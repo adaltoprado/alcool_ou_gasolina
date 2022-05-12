4f7306b5fdef0d883f89eb6dc8367d7bd28c4f18
Adalto Prado <adaltoprado@proton.me>
2022-05-12 19:27:47 -0300

    A implementar "viewBinding"

--- app/src/main/java/com/example/applcool_ou_gasolina/MainActivity.kt
+++ app/src/main/java/com/example/applcool_ou_gasolina/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.applcool_ou_gasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun calcularPreco(view: View){
+         //val precoAlcool=findViewById<EditText>(R.id.editPrecoAlcool)
+
     }
 }
